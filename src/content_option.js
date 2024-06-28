bd2e55042a4f4f3ee36797e0a3cb153e826acc91
iweenvishnu1352 <153094408+iweenvishnu1352@users.noreply.github.com>
2024-06-28 16:35:44 +0530

    files updated

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "VISHNU VARDHAN";
+const meta = {
+    title: "Vishnuvardhan",
+    description: "I’m Vishnu Vardhan _ Full stack devloper,currently working in Bangalore",
+};
+
+const introdata = {
+    title: "I’m Vishnu Vardhan",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        // third: "I develop mobile apps",
+    },
+    description: "UI Developer dedicated to crafting visually stunning and user-friendly digital experiences. Welcome to my portfolio, where creativity meets functionality.",
+    your_img_url: "https://lh3.googleusercontent.com/pw/AP1GczPGAuSxBFwLttTFw7IYCcsa02lt2DlFIJVZWc_KtbslQ49UTs_-PEyqG98pCRDPID1klDZbVym25MmKAYvAdunIEPBG3-a8fky-VEhFY5R3Z1jQRifW=w2400",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I am VISHNU VARDHAN, a UI Developer with 2 years of experience in crafting sophisticated, user-centric digital interfaces. I specialize in HTML, CSS, JavaScript, and React, transforming complex requirements into intuitive, visually appealing user experiences. My commitment to excellence and continuous learning ensures that I stay at the forefront of industry advancements, integrating the latest trends and technologies into my work",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "html",
+        value: 96,
+    },
+    {
+        name: "css",
+        value: 96,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name:'Node JS',
+        value:70,
+    },
+    {
+        name:'MongoDB',
+        value:60,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I specialize in website development and web app development, delivering high-quality, responsive, and user-friendly digital solutions. My expertise ensures robust functionality and seamless user experiences tailored to meet your unique business needs.",
+    },
+    // {
+    //     title: "Mobile Apps",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vishnuvardhan18100@gmail.com",
+    YOUR_FONE: "9989205550",
+    description: "I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision. Feel free to get in touch with me through the form below or via email. I look forward to connecting with you!",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
